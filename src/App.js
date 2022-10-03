002cfcbb06f948f215b9a595bb7dd9f9a67cc438
Arash Esfandiari <arashesfandiari@yahoo.com>
2022-10-03 16:45:20 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Row from './Row';
+import requests from './request';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Baba velam kon</h1>
+            <Row title="ARASHFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+            <Row title="Trending now" fetchUrl={requests.fetchTrending} />
+        </div>
+    );
 }
 
 export default App;
